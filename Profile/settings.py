cbb20e08a5b459fb9beb712ba0579e8b41f111d8
Mark-Ocampos-dev <markocampos.mail@gmail.com>
2025-02-14 10:00:38 +0800

    fix bugs 1

--- Profile/settings.py
+++ Profile/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 from decouple import config
-import dj_database_url
+# import dj_database_url
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,19 +56,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Profile.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default='#',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='#',
+#         conn_max_age=600
+#     )
+# }
 
 
 AUTH_PASSWORD_VALIDATORS = [
